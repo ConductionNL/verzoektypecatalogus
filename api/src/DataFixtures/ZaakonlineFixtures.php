1de837a08f7c677b7507d69cb6b66cdfb216d1dd
Robert Zondervan <rjzondervan@gmail.com>
2020-03-20 11:17:21 +0100

    Fix failing datafixture

--- api/src/DataFixtures/ZaakonlineFixtures.php
+++ api/src/DataFixtures/ZaakonlineFixtures.php
@@ -12,57 +12,57 @@ use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 class ZaakonlineFixtures extends Fixture
 {
 	private $params;
-	
+
 	public function __construct(ParameterBagInterface $params)
 	{
 		$this->params = $params;
 	}
-	
+
     public function load(ObjectManager $manager)
     {
     	// Lets make sure we only run these fixtures on huwelijksplanner enviroments
     	if (!in_array("zaakonline.nl", $this->params->get('app_domains'))) {
     		return false;
     	}
-    	
+
         /*
     	 *  Corona crisis
     	 */
-        
+
         // Verztype Babs andere gemeente
         $id = Uuid::fromString('90ad9841-bb57-44ee-beaa-b3e0a144626e');
         $request = new RequestType();
         $request->setSourceOrganization('https://wrc.zaakonline.nl/organizations/68b64145-0740-46df-a65a-9d3259c2fec8'); // dit moet de wrc verwijzing van utrecht zijn
         $request->setIcon('fas fa-user-tie');
         $request->setName('Ondersteunings aanvraag ZZP');
-        $request->setDescription('Met dit verzoek kunt u als ZZPer financielle hulp aanvragen');
-        $manager->persist($request);   
-        
+        $request->setDescription('Met dit verzoek kunt u als ZZPer financiele hulp aanvragen');
+        $manager->persist($request);
+
         // Dit is hacky tacky karig
-        $request->setId($id);        
-        $manager->persist($request);        
-        $manager->flush();        
-        $request = $manager->getRepository('App:RequestType')->findOneBy(array('id'=> $id));
+        $request->setId($id);
+        $manager->persist($request);
+        $manager->flush();
+        $request = $manager->getRepository('App:RequestType')->findOneBy(['id'=> $id]);
         // einde hacky tacky
-                
+
         $property = new Property();
         $property->setTitle('e-Mailadres');
         $property->setIcon('fal fa-user');
         $property->setType('string');
         $property->setFormat('email');
-        $property->setDescription('Op welk e-mailadres kunnen wij u berijken?');
+        $property->setDescription('Op welk e-mailadres kunnen wij u bereiken?');
         $property->setRequestType($request);
-        $manager->persist($property);        
-        
+        $manager->persist($property);
+
         $property = new Property();
         $property->setTitle('Telefoonnummer');
         $property->setIcon('fal fa-user');
         $property->setType('string');
         $property->setFormat('phone');
-        $property->setDescription('Op welk telefoonnummer kunnen wij u berijken?');
+        $property->setDescription('Op welk telefoonnummer kunnen wij u bereiken?');
         $property->setRequestType($request);
         $manager->persist($property);
-        
+
         $property = new Property();
         $property->setStart(true);
         $property->setSlug('noodvoorziening-corona');
@@ -73,7 +73,7 @@ class ZaakonlineFixtures extends Fixture
         $property->setDescription('Wat is het KVK nummer van der ondernemening waarvoor u aanvraagt?');
         $property->setRequestType($request);
         $manager->persist($property);
-        
+
         $property = new Property();
         $property->setTitle('BTW Uitstel');
         $property->setIcon('fal fa-user');
@@ -81,7 +81,7 @@ class ZaakonlineFixtures extends Fixture
         $property->setDescription('Wilt u BTW uitstel aanvragen?');
         $property->setRequestType($request);
         $manager->persist($property);
-        
+
         $property = new Property();
         $property->setTitle('Noodvoorziening');
         $property->setIcon('fal fa-user');
@@ -89,7 +89,7 @@ class ZaakonlineFixtures extends Fixture
         $property->setDescription('Wilt u een noodvoorziening van 4000 aanvragen?');
         $property->setRequestType($request);
         $manager->persist($property);
-        
+
         $property = new Property();
         $property->setTitle('Medewerkers');
         $property->setIcon('fal fa-user');
@@ -97,7 +97,7 @@ class ZaakonlineFixtures extends Fixture
         $property->setDescription('Heeft u medewerkers in dienst?');
         $property->setRequestType($request);
         $manager->persist($property);
-        
+
         $property = new Property();
         $property->setTitle('Toelichting');
         $property->setIcon('fal fa-user');
@@ -106,8 +106,8 @@ class ZaakonlineFixtures extends Fixture
         $property->setDescription('Kunt u een toelichting op uw aanvraag geven?');
         $property->setRequestType($request);
         $manager->persist($property);
-        
-        
+
+
         // Verztype Babs andere gemeente
         $id = Uuid::fromString('d78197df-af77-4b5e-bed0-054cba047550');
         $request = new RequestType();
@@ -116,7 +116,14 @@ class ZaakonlineFixtures extends Fixture
         $request->setName('Aanvraag bijzonder bijstand');
         $request->setDescription('Met dit verzoek kunt bijzondere bijstand aanvragen');
         $manager->persist($request);
-        
+
+        // Dit is hacky tacky karig
+        $request->setId($id);
+        $manager->persist($request);
+        $manager->flush();
+        $request = $manager->getRepository('App:RequestType')->findOneBy(['id'=> $id]);
+        // einde hacky tacky
+
         $property = new Property();
         $property->setTitle('e-Mailadres');
         $property->setIcon('fal fa-user');
@@ -125,14 +132,14 @@ class ZaakonlineFixtures extends Fixture
         $property->setDescription('Op welk e-mailadres kunnen wij u berijken?');
         $property->setRequestType($request);
         $manager->persist($property);
-        
+
         // Dit is hacky tacky karig
         $request->setId($id);
         $manager->persist($request);
         $manager->flush();
         $request = $manager->getRepository('App:RequestType')->findOneBy(array('id'=> $id));
         // einde hacky tacky
-        
+
         $property = new Property();
         $property->setTitle('Telefoonnummer');
         $property->setIcon('fal fa-user');
@@ -141,7 +148,7 @@ class ZaakonlineFixtures extends Fixture
         $property->setDescription('Op welk telefoonnummer kunnen wij u berijken?');
         $property->setRequestType($request);
         $manager->persist($property);
-        
+
         $property = new Property();
         $property->setTitle('Toelichting');
         $property->setIcon('fal fa-user');
@@ -150,7 +157,7 @@ class ZaakonlineFixtures extends Fixture
         $property->setDescription('Kunt u een toelichting op uw aanvraag geven?');
         $property->setRequestType($request);
         $manager->persist($property);
-        
+
         // Verztype Babs andere gemeente
         $id = Uuid::fromString('64127a29-c452-4600-a9a0-b3f827b4d2e5');
         $request = new RequestType();
@@ -159,14 +166,14 @@ class ZaakonlineFixtures extends Fixture
         $request->setName('Ik wil helpen');
         $request->setDescription('Met dit verzoek kunt u aangeven dat u wilt bijdragen');
         $manager->persist($request);
-        
+
         // Dit is hacky tacky karig
         $request->setId($id);
         $manager->persist($request);
         $manager->flush();
         $request = $manager->getRepository('App:RequestType')->findOneBy(array('id'=> $id));
         // einde hacky tacky
-        
+
         $property = new Property();
         $property->setTitle('e-Mailadres');
         $property->setIcon('fal fa-user');
@@ -175,7 +182,7 @@ class ZaakonlineFixtures extends Fixture
         $property->setDescription('Op welk e-mailadres kunnen wij u berijken?');
         $property->setRequestType($request);
         $manager->persist($property);
-        
+
         $property = new Property();
         $property->setTitle('Telefoonnummer');
         $property->setIcon('fal fa-user');
@@ -184,8 +191,8 @@ class ZaakonlineFixtures extends Fixture
         $property->setDescription('Op welk telefoonnummer kunnen wij u berijken?');
         $property->setRequestType($request);
         $manager->persist($property);
-        
-        
+
+
         // Verztype Babs andere gemeente
         $id = Uuid::fromString('2201ebbd-f2b0-4ece-88e0-18ba0d2949e0');
         $request = new RequestType();
@@ -194,14 +201,14 @@ class ZaakonlineFixtures extends Fixture
         $request->setName('Ik heb hulp nodig');
         $request->setDescription('Met dit verzoek kunt u aangeven dat u hulp nodig heeft');
         $manager->persist($request);
-        
+
         // Dit is hacky tacky karig
         $request->setId($id);
         $manager->persist($request);
         $manager->flush();
         $request = $manager->getRepository('App:RequestType')->findOneBy(array('id'=> $id));
         // einde hacky tacky
-        
+
         $property = new Property();
         $property->setTitle('e-Mailadres');
         $property->setIcon('fal fa-user');
@@ -210,7 +217,7 @@ class ZaakonlineFixtures extends Fixture
         $property->setDescription('Op welk e-mailadres kunnen wij u berijken?');
         $property->setRequestType($request);
         $manager->persist($property);
-        
+
         $property = new Property();
         $property->setTitle('Telefoonnummer');
         $property->setIcon('fal fa-user');
@@ -219,7 +226,7 @@ class ZaakonlineFixtures extends Fixture
         $property->setDescription('Op welk telefoonnummer kunnen wij u berijken?');
         $property->setRequestType($request);
         $manager->persist($property);
-        
+
 		$manager->flush();
     }
 }
