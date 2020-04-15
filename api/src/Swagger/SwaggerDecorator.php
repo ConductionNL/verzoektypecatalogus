8330b41fbff82741de58490ee710c0dd73442d06
Robert Zondervan <rjzondervan@gmail.com>
2020-04-15 09:33:23 +0200

    Revert "Merge remote-tracking branch 'upstream/master' into dev-robert"
    This reverts commit 6b1b694d62d6b66b137e7f2003e978bd97c4bb4a.

--- api/src/Swagger/SwaggerDecorator.php
+++ api/src/Swagger/SwaggerDecorator.php
@@ -146,7 +146,7 @@ final class SwaggerDecorator implements NormalizerInterface
 
         $docs['definitions'] = $additionalDocs;
 
-        
+        /*
         // Lest add an host
         if ($this->params->get('common_ground.oas.host')) {
             $docs['host'] = $this->params->get('common_ground.oas.host');
@@ -158,19 +158,18 @@ final class SwaggerDecorator implements NormalizerInterface
         }
 
         if($this->params->get('app_env') == 'prod'){
-            $docs['servers'][] = ['name'=>$this->params->get('app_domain'),'url'=>''.$this->params->get('app_name').'.'.$this->params->get('app_domain'),];
+            $docs['servers'][$this->params->get('app_domain')] = ['name'=>$this->params->get('app_domain'),['url'=>''.$this->params->get('app_name').'.'.$this->params->get('app_domain'),];
         }
         else{
-            $docs['servers'][] = ['name'=>$this->params->get('app_domain'),'url'=>''.$this->params->get('app_name').'.'.$this->params->get('app_env').'.'.$this->params->get('app_domain'),];
+            $docs['servers'][$this->params->get('app_domain')] = ['name'=>$this->params->get('app_domain'),['url'=>''.$this->params->get('app_name').'.'.$this->params->get('app_env').'.'.$this->params->get('app_domain'),];
         }
-
-
+        */
         /*
         app_domain
         foreach ($this->params->get('common_ground.oas.servers') as $key => $value) {
             $docs['servers'][$key] = $value;
         }
-        */
+        /*
 
 
 
@@ -458,7 +457,7 @@ final class SwaggerDecorator implements NormalizerInterface
                         $groups = $propertyAnnotation->getGroups();
                         break;
 
-                    // Constrainds (Validation)
+                        // Constrainds (Validation)
                     case "Assert\Date":
                         $atributes['type'] = 'string';
                         $atributes['format'] = 'date';
