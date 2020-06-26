03d786f1a46d0f99dba0eb281cd74e28b6e0cfbc
Ruben van der Linde <ruben@conduction.nl>
2020-06-26 10:05:02 +0200

    Extra properties voor verhuizen

--- api/src/DataFixtures/SHertogenboschFixtures.php
+++ api/src/DataFixtures/SHertogenboschFixtures.php
@@ -79,14 +79,85 @@ class SHertogenboschFixtures extends Fixture
         $manager->flush();
         $property = $manager->getRepository('App:Property')->findOneBy(['id' => $id]);
 
+        $id = Uuid::fromString('58728f7c-cde3-4faa-8c7d-15c3b2cddcb5');
+        $property = new Property();
+        $property->setTitle('Wie');
+        $property->setIcon('fal fa-map-marked');
+        $property->setType('array');
+        $property->setFormat('bag');
+        $property->setRequired(true);
+        $property->setDescription('Wie gaan er verhuizen');
+        $property->setRequestType($requestType);
+        $manager->persist($property);
+        $property->setId($id);
+        $manager->persist($property);
+        $manager->flush();
+        $property = $manager->getRepository('App:Property')->findOneBy(['id' => $id]);
+
+        $id = Uuid::fromString('39402814-8960-47a4-b107-863f20d629ae');
+        $property = new Property();
+        $property->setTitle('Wiebsn');
+        $property->setIcon('fal fa-map-marked');
+        $property->setType('array');
+        $property->setFormat('bsn');
+        $property->setDescription('BSN nummers van alle verhuisenden');
+        $property->setRequestType($requestType);
+        $manager->persist($property);
+        $property->setId($id);
+        $manager->persist($property);
+        $manager->flush();
+        $property = $manager->getRepository('App:Property')->findOneBy(['id' => $id]);
+
+        $id = Uuid::fromString('32061b32-1f8d-4bd7-b203-52b22585f3c9');
+        $property = new Property();
+        $property->setTitle('Email');
+        $property->setIcon('fal fa-map-marked');
+        $property->setType('string');
+        $property->setFormat('email');
+        $property->setDescription('Op welk E-Mail adders kunnen we u berijken?');
+        $property->setRequestType($requestType);
+        $manager->persist($property);
+        $property->setId($id);
+        $manager->persist($property);
+        $manager->flush();
+        $property = $manager->getRepository('App:Property')->findOneBy(['id' => $id]);
+
+        $id = Uuid::fromString('09cac491-a428-47eb-99ac-9717b1690620');
+        $property = new Property();
+        $property->setTitle('Telefoon');
+        $property->setIcon('fal fa-map-marked');
+        $property->setType('string');
+        $property->setFormat('phonenumber');
+        $property->setDescription('Op welk telefoon nummer kunnen we u berijken?');
+        $property->setRequestType($requestType);
+        $manager->persist($property);
+        $property->setId($id);
+        $manager->persist($property);
+        $manager->flush();
+        $property = $manager->getRepository('App:Property')->findOneBy(['id' => $id]);
+
+        $id = Uuid::fromString('f1964c98-df49-431a-a5e1-64c17d7d956b');
+        $property = new Property();
+        $property->setTitle('notificatie');
+        $property->setIcon('fal fa-map-marked');
+        $property->setType('boolean');
+        $property->setFormat('bsn');
+        $property->setDescription('Mogen wij andere op de hoogte stellen van uw verhuizing');
+        $property->setRequestType($requestType);
+        $manager->persist($property);
+        $property->setId($id);
+        $manager->persist($property);
+        $manager->flush();
+        $property = $manager->getRepository('App:Property')->findOneBy(['id' => $id]);
+
         // Bijbehorende taken die in de queu worden gezet
         $task = new Task();
         $task->setRequestType($requestType);
-        $task->setName('Aanroepen webhook');
+        $task->setName('Updaten burger service nummers');
         $task->setDescription('Deze task roept een webhook aan als er een verzoek vanhet type verhuizen wordt gecrieërd');
-        $task->setEndpoint('https://webhook.mijncluster.nl');
+        $task->setEndpoint($this->commonGroundService->cleanUrl(["component"=>"vs","type"=>"webhook"]));
         $task->setType('GET');
-        $task->setCode('webhook_call_pink');
+        $task->setCode('set_bsn');
         $task->setEvent('create');
         $task->setTimeInterval('P0D');
         $manager->persist($task);
