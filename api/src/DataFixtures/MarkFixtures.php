5462e35f1eb72d91eea77e82cff275044d9003a5
Mark Bout <markhendrikbout@gmail.com>
2020-07-28 16:31:36 +0200

    Ja/Nee sticker formulier gemaakt

--- api/src/DataFixtures/MarkFixtures.php
+++ api/src/DataFixtures/MarkFixtures.php
@@ -27,67 +27,119 @@ class MarkFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        $id = Uuid::fromString('e0db78d8-8324-46e1-bad8-991bfb11c8f2');
+        //JA/NEE sticker bestellen
+
+        //reqeust type
+        $id = Uuid::fromString('7e3998c0-4e9d-41e2-b9dc-f0840efc44d9');
         $requestType = new RequestType();
-        $requestType->setOrganization('002220647');
-        $requestType->setName("Contact formulier");
-        $requestType->setDescription('Neem via dit formulier contact met ons op');
+        $requestType->setName("Ja/Nee sticker bestellen");
+        $requestType->setDescription('Bestel via dit formulier een JA/NEE of NEE/NEE sticker.');
         $manager->persist($requestType);
         $requestType->setId($id);
         $manager->flush();
         $requestType = $manager->getRepository('App:RequestType')->findOneBy(['id' => $id]);
 
-        //onderwerp contact
-        $id = Uuid::fromString('d7d1bd3c-ac1b-4816-8a50-d06f0d5f914f');
+        //type sticker
+        $id = Uuid::fromString('c77cb790-7a93-46c6-a280-7f35bb29097f');
         $property = new Property();
-        $property->setTitle('Onderwerp');
+        $property->setTitle('Welke sticker wilt u hebben?');
+        $property->setType('string');
+        $property->setFormat('radio');
+        $property->setRequired(true);
+        $property->setEnum(['NEE/JA (u krijgt geen reclamedrukwerk, maar wel huis-aan-huisbladen)', 'NEE/NEE (u krijgt geen reclamedrukwerk en geen huis-aan-huisbladen)']);
+        $property->setRequestType($requestType);
+        $manager->persist($property);
+        $property->setId($id);
+        $manager->persist($property);
+        $manager->flush();
+
+        //Voornaam en achternaam
+        $id = Uuid::fromString('2ae7bd21-117c-4451-94ab-cff5727cbc96');
+        $property = new Property();
+        $property->setTitle('Voornaam en achternaam:');
         $property->setType('string');
         $property->setFormat('string');
-        $property->setDescription("Waar gaat uw vraag over?");
+        $property->setRequired(true);
         $property->setRequestType($requestType);
         $manager->persist($property);
         $property->setId($id);
         $manager->persist($property);
         $manager->flush();
 
-        //beschrijving contact
-        $id = Uuid::fromString('541240d2-1760-4377-b3bb-f25278c0b09e');
+        //Postcode
+        $id = Uuid::fromString('c9ed0d31-6296-4e65-9416-aa2a1c366ecd');
         $property = new Property();
-        $property->setTitle('beschrijving');
+        $property->setTitle('Postcode:');
         $property->setType('string');
-        $property->setFormat('textarea');
-        $property->setDescription("Beschrijf uw vraag");
+        $property->setFormat('string');
+        $property->setRequired(true);
         $property->setRequestType($requestType);
         $manager->persist($property);
         $property->setId($id);
         $manager->persist($property);
         $manager->flush();
 
-        //email contact
+        //huisnummer
+        $id = Uuid::fromString('f0ab2b61-2a98-48c0-b984-58ab5fa8568f');
+        $property = new Property();
+        $property->setTitle('Huisnummer:');
+        $property->setType('integer');
+        $property->setFormat('string');
+        $property->setRequired(true);
+        $property->setRequestType($requestType);
+        $manager->persist($property);
+        $property->setId($id);
+        $manager->persist($property);
+        $manager->flush();
+
+        //huisnummer toevoeging
+        $id = Uuid::fromString('afcf73a4-9d31-4104-baf2-039c0fae85e2');
+        $property = new Property();
+        $property->setTitle('Huisnummer toevoeging:');
+        $property->setType('string');
+        $property->setFormat('string');
+        $property->setRequestType($requestType);
+        $manager->persist($property);
+        $property->setId($id);
+        $manager->persist($property);
+        $manager->flush();
+
+        //straatnaam
+        $id = Uuid::fromString('a48ca6a1-0dbe-4497-9034-8760e407c662');
+        $property = new Property();
+        $property->setTitle('Straatnaam:');
+        $property->setType('string');
+        $property->setFormat('string');
+        $property->setRequired(true);
+        $property->setRequestType($requestType);
+        $manager->persist($property);
+        $property->setId($id);
+        $manager->persist($property);
+        $manager->flush();
+
+        //Woonplaats
+        $id = Uuid::fromString('5fc37fbe-14e8-48cf-ad32-0de22fa3bc57');
+        $property = new Property();
+        $property->setTitle('Woonplaats:');
+        $property->setType('string');
+        $property->setFormat('string');
+        $property->setRequired(true);
+        $property->setRequestType($requestType);
+        $manager->persist($property);
+        $property->setId($id);
+        $manager->persist($property);
+        $manager->flush();
+
+        //Emailadres
         $id = Uuid::fromString('825b1050-6dde-4d17-b180-6bde3204364f');
         $property = new Property();
-        $property->setTitle('email');
+        $property->setTitle('E-mailadres:');
         $property->setType('string');
         $property->setFormat('email');
-        $property->setDescription("wat is uw email adres");
         $property->setRequestType($requestType);
         $manager->persist($property);
         $property->setId($id);
         $manager->persist($property);
         $manager->flush();
-
-        //telefoon contact
-        $id = Uuid::fromString('6aed6cf9-86d5-48fe-af41-57a84e159ffe');
-        $property = new Property();
-        $property->setTitle('Telefoonnummer');
-        $property->setType('string');
-        $property->setFormat('string');
-        $property->setDescription("Uw email adres");
-        $property->setRequestType($requestType);
-        $manager->persist($property);
-        $property->setId($id);
-        $manager->persist($property);
-        $manager->flush();
-
     }
 }
