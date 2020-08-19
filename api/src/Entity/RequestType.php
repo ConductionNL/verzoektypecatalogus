70ffc640402b51cb06c0c3be35b9958a51837bcb
ginokok1996 <ginokok1996@hotmail.com>
2020-08-19 10:45:12 +0200

    update

--- api/src/Entity/RequestType.php
+++ api/src/Entity/RequestType.php
@@ -95,10 +95,13 @@ class RequestType
      * @example https://wrc.zaakonline.nl/organisations/16353702-4614-42ff-92af-7dd11c8eef9f
      *
      * @Gedmo\Versioned
+     * @Assert\Length(
+     *     max = 255
+     * )
      * @Assert\NotNull
      * @Assert\Url
-     * @Groups({"read", "write"})
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @Groups({"read","write"})
+     * @ORM\Column(type="string", length=255)
      */
     private $organization;
 
