ae8c6d38825528dabcee05c3b59018872ae1c7af
ginokok1996 <ginokok1996@users.noreply.github.com>
2020-09-09 09:01:52 +0000

    Apply fixes from StyleCI

--- api/src/DataFixtures/WestFrieslandFixtures.php
+++ api/src/DataFixtures/WestFrieslandFixtures.php
@@ -276,7 +276,7 @@ class WestFrieslandFixtures extends Fixture
         $property->setType('string');
         $property->setFormat('url');
         $property->setIri('cc/people');
-        $property->setConfiguration(['email'=>false,'telephone'=>false,'givenName'=>true,'familyName'=>true,'birthday'=>true,'placeOfBirth'=>true]);
+        $property->setConfiguration(['email'=>false, 'telephone'=>false, 'givenName'=>true, 'familyName'=>true, 'birthday'=>true, 'placeOfBirth'=>true]);
         $property->setRequestType($requestType);
         $manager->persist($property);
         $property->setId($id);
