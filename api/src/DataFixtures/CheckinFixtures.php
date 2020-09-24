1294ec77312fadb6a408f7c425c3ef60b27ca63f
Ruben van der Linde <ruben@conduction.nl>
2020-09-24 15:04:14 +0200

    Textual fix

--- api/src/DataFixtures/CheckinFixtures.php
+++ api/src/DataFixtures/CheckinFixtures.php
@@ -71,7 +71,6 @@ class CheckinFixtures extends Fixture
         $property->setName('khn');
         $property->setType('string');
         $property->setFormat('text');
-        $property->setDescription('Als u lid bent van koningklijke horeca nederland kunt u hier uw lidmaarschats nummer opvoeren voor korting');
         $property->setRequestType($requestType);
 
         $manager->persist($property);
